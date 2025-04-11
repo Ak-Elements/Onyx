d942154e2365a2b28faebe2a07f67d6c3f6aaed6
akElements <akelements.dev@gmail.com>
2025-04-11 13:41:02 +0200

    Pulling shaderc from git if it's not available with vulkan install

--- onyx/modules/graphics/dependencies.cmake
+++ onyx/modules/graphics/dependencies.cmake
@@ -4,6 +4,55 @@ message(STATUS "[${CURRENT_TARGET}] Getting dependencies.")
 
 find_package(Vulkan REQUIRED COMPONENTS shaderc_combined)
 
+if (NOT Vulkan_shaderc_combined_FOUND)
+    message(WARNING "Vulkan shaderc not found. Consider installing it for shader compilation support.")
+    message(WARNING "Getting shaderc from git instead.")
+
+    CPMAddPackage(
+        NAME SPIRV-Headers
+        GITHUB_REPOSITORY KhronosGroup/SPIRV-Headers
+        GIT_TAG 54a521dd130ae1b2f38fef79b09515702d135bdd
+        EXCLUDE_FROM_ALL YES
+    )
+
+    CPMAddPackage(
+        NAME SPIRV-Tools
+        GITHUB_REPOSITORY KhronosGroup/SPIRV-Tools
+        GIT_TAG f289d047f49fb60488301ec62bafab85573668cc
+        OPTIONS
+            SPIRV_SKIP_EXECUTABLES ON
+            SPIRV_SKIP_TESTS ON
+        EXCLUDE_FROM_ALL YES
+    )
+
+    CPMAddPackage(
+        NAME glslang
+        GITHUB_REPOSITORY KhronosGroup/glslang
+        GIT_TAG 8b822ee8ac2c3e52926820f46ad858532a895951
+        EXCLUDE_FROM_ALL YES
+    )
+
+    CPMAddPackage(
+        NAME shaderc
+        GITHUB_REPOSITORY google/shaderc
+        GIT_TAG 0968768c61d4eb7dd861114412e904bb3d59b7b6
+        OPTIONS 
+            SHADERC_SKIP_TESTS ON
+        EXCLUDE_FROM_ALL YES
+    )
+
+    target_include_directories(shaderc_combined
+    PUBLIC
+        $<BUILD_INTERFACE:"${shaderc_SOURCE_DIR}/libshaderc/include">
+        $<INSTALL_INTERFACE:include/shaderc>
+    )
+
+    add_library(Vulkan::shaderc_combined ALIAS shaderc_combined)
+
+    install(TARGETS shaderc_combined EXPORT onyx-graphics-targets)
+    
+endif()
+
 CPMAddPackage(
 	NAME VulkanMemoryAllocator
     GITHUB_REPOSITORY GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator
