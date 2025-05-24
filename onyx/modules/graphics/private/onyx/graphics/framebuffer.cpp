cfe375cba341ebe43b265070a2a1d096a74cec75
akElements <akelements.dev@gmail.com>
2025-05-24 14:07:37 +0200

    Fixed wrong hash calculation in framebuffer

--- onyx/modules/graphics/private/onyx/graphics/framebuffer.cpp
+++ onyx/modules/graphics/private/onyx/graphics/framebuffer.cpp
@@ -7,10 +7,10 @@ namespace Onyx::Graphics
     onyxU64 FramebufferSettings::Hash() const
     {
         onyxU64 hash = 0;
-        Hash::FNV1aHash64(reinterpret_cast<const onyxU8*>(&m_Width), sizeof(onyxU32), hash);
-        Hash::FNV1aHash64(reinterpret_cast<const onyxU8*>(&m_Height), sizeof(onyxU32), hash);
-        Hash::FNV1aHash64(reinterpret_cast<const onyxU8*>(&m_LayerCount), sizeof(onyxU32), hash);
-        Hash::FNV1aHash64(reinterpret_cast<onyxU8*>(m_ColorTargets.size()), sizeof(onyxU64), hash);
+        hash = Hash::FNV1aHash64(reinterpret_cast<const onyxU8*>(&m_Width), sizeof(onyxU32), hash);
+        hash = Hash::FNV1aHash64(reinterpret_cast<const onyxU8*>(&m_Height), sizeof(onyxU32), hash);
+        hash = Hash::FNV1aHash64(reinterpret_cast<const onyxU8*>(&m_LayerCount), sizeof(onyxU32), hash);
+        hash = Hash::FNV1aHash64(reinterpret_cast<onyxU8*>(m_ColorTargets.size()), sizeof(onyxU64), hash);
         // include color and depth target in hash
         //Hash::FNV1aHash64(reinterpret_cast<onyxU8*>(m_DepthTarget), sizeof(onyxU64), hash);
 
