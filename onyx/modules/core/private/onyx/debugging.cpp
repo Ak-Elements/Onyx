9172b3084409643a43410024dc4d2f2d225c7526
Borna Šoštarić <sostaric.borna.dev@hotmail.com>
2025-09-12 14:13:51 +0200

    implement IsDebuggerPresent for linux

--- onyx/modules/core/private/onyx/debugging.cpp
+++ onyx/modules/core/private/onyx/debugging.cpp
@@ -1,15 +1,28 @@
 #include <onyx/debugging.h>
 
+#if ONYX_IS_WINDOWS // vvv ONYX_IS_WINDOWS vvv
+
 #pragma comment(lib, "kernel32.lib")
 
 extern "C" void DebugBreak();
 extern "C" int IsDebuggerPresent();
+
+#endif // ^^^ ONYX_IS_WINDOWS ^^^
 
 #if !ONYX_IS_WINDOWS
 
 #include <csignal>
 
 #endif
+
+#if ONYX_IS_LINUX // vvv ONYX_IS_LINUX vvv
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#endif // ^^^ ONYX_IS_LINUX ^^^
+
 
 namespace Onyx
 {
@@ -48,13 +61,43 @@ namespace Onyx
 
         return ::IsDebuggerPresent();
 
-        #else // ^^^ ONYX_IS_WINDOWS ^^^ || vvv !ONYX_IS_WINDOWS vvv
+        #elif ONYX_IS_LINUX // ^^^ ONYX_IS_WINDOWS ^^^ || vvv ONYX_IS_LINUX vvv
+
+        // ! Possibility of hitting a no-fs linux system
+
+        auto *proc_status = ::fopen("/proc/self/status", "r");
+        if (proc_status != nullptr) {
+            // ? Log this
+            return false;
+        }
+
+        char *line = nullptr;
+        ::size_t line_len = 0;
+        auto token_str = "TracerPid:";
+        auto is_debugger_present = false;
+
+        while (::getline(&line, &line_len, proc_status) != -1) {
+            char *token_pos = ::strstr(line, token_str);
+            if (token_pos == nullptr) {
+                continue;
+            }
+
+            is_debugger_present = ::atoi(token_pos + ::strlen(token_str)) != 0;
+            break;
+        }
+
+        ::free(line);
+        ::fclose(proc_status);
+
+        return is_debugger_present;
+
+        #else // ^^^ ONYX_IS_LINUX ^^^ || vvv !ONYX_IS_WINDOWS && !ONYX_IS_LINUX vvv
 
         #warning "IsDebuggerPresent() is not implemented for this platform"
 
         return false;
 
-        #endif // ^^^ !ONYX_IS_WINDOWS ^^^
+        #endif // ^^^ !ONYX_IS_WINDOWS && !ONYX_IS_LINUX ^^^
 
         #endif // ^^^ !ONYX_IS_DEBUG ^^^
     }
