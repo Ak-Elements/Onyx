faf061753a346698d145c12f40234a72240e587c
sphere <sostaric.borna.dev@hotmail.com>
2026-02-26 20:13:48 +0100

    cache presence of debugger
    NOTE: will return false if debugger was attached afterwards

--- onyx/modules/core/private/onyx/debugging.cpp
+++ onyx/modules/core/private/onyx/debugging.cpp
@@ -56,45 +56,50 @@ namespace Onyx
 
 #if ONYX_IS_WINDOWS // vvv ONYX_IS_WINDOWS vvv
 
-        return ::IsDebuggerPresent();
+        static bool is_debugger_present = [] {
+          return ::IsDebuggerPresent();
+        }();
 
 #elif ONYX_IS_LINUX // ^^^ ONYX_IS_WINDOWS ^^^ || vvv ONYX_IS_LINUX vvv
 
-        // ! Possibility of hitting a no-fs linux system
-
-        auto *proc_status = ::fopen("/proc/self/status", "r");
-        if (proc_status != nullptr) {
-            // ? Log this
-            return false;
-        }
-
-        char *line = nullptr;
-        ::size_t line_len = 0;
-        auto token_str = "TracerPid:";
-        auto is_debugger_present = false;
-
-        while (::getline(&line, &line_len, proc_status) != -1) {
-            char *token_pos = ::strstr(line, token_str);
-            if (token_pos == nullptr) {
-                continue;
+        static bool is_debugger_present = [] {
+            // ! Possibility of hitting a no-fs linux system
+            auto *proc_status = ::fopen("/proc/self/status", "r");
+            if (proc_status != nullptr) {
+                // ? Log this
+                return false;
             }
 
-            is_debugger_present = ::atoi(token_pos + ::strlen(token_str)) != 0;
-            break;
-        }
+            char *line = nullptr;
+            ::size_t line_len = 0;
+            auto token_str = "TracerPid:";
+            auto is_debugger_present = false;
 
-        ::free(line);
-        ::fclose(proc_status);
+            while (::getline(&line, &line_len, proc_status) != -1) {
+                char *token_pos = ::strstr(line, token_str);
+                if (token_pos == nullptr) {
+                    continue;
+                }
 
-        return is_debugger_present;
+                is_debugger_present = ::atoi(token_pos + ::strlen(token_str)) != 0;
+                break;
+            }
+
+            ::free(line);
+            ::fclose(proc_status);
+
+            return is_debugger_present;
+        }();
 
 #else // ^^^ ONYX_IS_LINUX ^^^ || vvv !ONYX_IS_WINDOWS && !ONYX_IS_LINUX vvv
 
 #warning "IsDebuggerPresent() is not implemented for this platform"
 
-        return false;
+        static bool is_debugger_present = false;
 
 #endif // ^^^ !ONYX_IS_WINDOWS && !ONYX_IS_LINUX ^^^
+
+        return is_debugger_present;
 
 #endif // ^^^ !ONYX_IS_DEBUG ^^^
     }
